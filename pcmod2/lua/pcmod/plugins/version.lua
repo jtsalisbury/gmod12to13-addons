81e1fdf9c012fbe8e5685f9b983102cfe9da9c09
crazyscouter <crazyscouter1@gmail.com>
2014-05-04 17:35:08 -0700

    Fixed Version Checking

--- pcmod2/lua/pcmod/plugins/version.lua
+++ pcmod2/lua/pcmod/plugins/version.lua
@@ -14,31 +14,26 @@ function PLUGIN:AfterLoad()
 		PCMod.Msg("+-+-+-+-+-+-+-+-+-   ERROR   -+-+-+-+-+-+-+-+-+")  
 		PCMod.Msg("Could not find  PCMod version text file! Please make sure your addon is named: pcmod2 & the version file is name version.txt!")  
 		PCMod.Msg("+-+-+-+-+-+-+-+-+- END ERROR -+-+-+-+-+-+-+-+-+")  
-		return;
+	else
+	
+		local vers = file.Read("addons/pcmod2/version.txt", "GAME");
+		
+		http.Fetch("https://raw.githubusercontent.com/crazyscouter/GMod-Addons/master/pcmod2/version.txt",
+			function(body, len, headers, code)
+				if (vers != body) then
+					PCMod.Msg("+-+-+-+-+-+-+-+-+-   WARNING   -+-+-+-+-+-+-+-+-+")  
+					PCMod.Msg("There is a new version of PCMod2 out! Update now at: https://github.com/crazyscouter/GMod-Addons")  
+					PCMod.Msg("+-+-+-+-+-+-+-+-+- END WARNING -+-+-+-+-+-+-+-+-+")  
+				else
+					PCMod.Msg( ">> PCMod is fully updated! <<" )
+				end
+			end,
+			function(err)
+				PCMod.Msg("+-+-+-+-+-+-+-+-+-   ERROR   -+-+-+-+-+-+-+-+-+")  
+				PCMod.Msg(""..err.."")  
+				PCMod.Msg("+-+-+-+-+-+-+-+-+- END ERROR -+-+-+-+-+-+-+-+-+")  
+				return;
+			end
+		);
 	end
-	
-	local vers = file.Read("addons/pcmod2/version.txt", "GAME");
-	local remote_version;
-	
-	http.Fetch("https://raw.githubusercontent.com/crazyscouter/GMod-Addons/master/pcmod2/addon.txt",
-		function(body, len, headers, code)
-			remote_version = body;
-		end,
-		function(err)
-			PCMod.Msg("+-+-+-+-+-+-+-+-+-   ERROR   -+-+-+-+-+-+-+-+-+")  
-			PCMod.Msg(""..err.."")  
-			PCMod.Msg("+-+-+-+-+-+-+-+-+- END ERROR -+-+-+-+-+-+-+-+-+")  
-			return;
-		end
-	);
-	
-	if (vers != remote_version) then
-		PCMod.Msg("+-+-+-+-+-+-+-+-+-   WARNING   -+-+-+-+-+-+-+-+-+")  
-		PCMod.Msg("There is a new version of PCMod2 out! Update now at: https://github.com/crazyscouter/GMod-Addons")  
-		PCMod.Msg("+-+-+-+-+-+-+-+-+- END WARNING -+-+-+-+-+-+-+-+-+")  
-		return;
-	end
-	
-	PCMod.Msg( ">> PCMod is fully updated! <<" )
 end
-
