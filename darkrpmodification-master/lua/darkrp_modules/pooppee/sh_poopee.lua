cd731370974a84745b0cec4ef754a83467b7230c
crazyscouter <crazyscouter1@gmail.com>
2014-05-07 17:15:51 -0700

    Added PooPee for DarkRP 2.5

---
+++ darkrpmodification-master/lua/darkrp_modules/pooppee/sh_poopee.lua
@@ -0,0 +1,17 @@
+PooPee = {}
+
+DarkRP.declareChatCommand{
+	command = "poo",
+	description = "Take a poo!",
+	delay = 0.5
+}
+DarkRP.declareChatCommand{
+	command = "poop",
+	description = "Take a poop!",
+	delay = 0.5
+}
+DarkRP.declareChatCommand{
+	command = "pee",
+	description = "Take a pee!",
+	delay = 0.5
+}
